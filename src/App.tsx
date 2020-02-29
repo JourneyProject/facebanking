eb43fc58f0d77027f535dbe4b393fccdda835a3b
Chris Antaki <ChrisAntaki@gmail.com>
2020-02-29 10:20:37 -0800

    Prototypes static app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+// @ts-ignore: No types available
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
+import logo from './journey-logo.png';
 
 
 function openFacebookMessenger() {
@@ -7,13 +10,36 @@ function openFacebookMessenger() {
 }
 
 function App() {
+  const [copied, setCopied] = useState(false);
+
+  const message = 'Hey, I saw you liked Bernie on Facebook! Have you considered volunteering with him before the election?';
+
   return (
     <div className="App">
-      <div>Graphic</div>
-      <div>Calls to action</div>
-      <div>Text to copy to clipboard</div>
-      <div className="open-facebook"
-           onClick={openFacebookMessenger}>
+      <div>
+        <img className="Logo" alt="Bernie logo" src={logo} />
+      </div>
+
+      <div className="Copy">
+        <div className="Copy-copied"
+          hidden={!copied}>
+          Copied!
+        </div>
+        <CopyToClipboard text={message}
+          onCopy={() => {
+            // Restart animation.
+            setCopied(false);
+            setTimeout(() => void setCopied(true), 100);
+          }}>
+          <div>
+            <div>Click below to copy the prompt.</div>
+            <div className="Copy-preview">{message}</div>
+          </div>
+        </CopyToClipboard>
+      </div>
+
+      <div className="OpenFacebook"
+        onClick={openFacebookMessenger}>
         Open Facebook Messenger
       </div>
     </div>
