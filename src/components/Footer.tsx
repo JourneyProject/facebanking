ddc95fee28db39bb86bdf0c7377622978806226f
Chris Antaki <ChrisAntaki@gmail.com>
2020-03-01 17:03:59 -0800

    Follows suggestions from React compiler

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
         <Container fluid className="footer-wrapper">
             <Row className="no-gutters">
                 <Col className="callout col-12">
-                    <p>Made with <i>♥️</i> by <a href="https://github.com/JourneyProject" target="_blank">Journey</a>&nbsp;&nbsp;|&nbsp;&nbsp;© 2020 Journey, All rights reserved.</p>
+                    <p>Made with <span className="font-italic" role="img" aria-label="Heart">♥️</span> by <a href="https://github.com/JourneyProject" target="_blank" rel="noopener noreferrer">Journey</a>&nbsp;&nbsp;|&nbsp;&nbsp;© 2020 Journey, All rights reserved.</p>
                 </Col>
             </Row>
         </Container>
